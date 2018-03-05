c0e9adebbd2f5351f7102580dfa70f152fdc4fc4
Cindy Zhang <cindyzyx9@gmail.com>
2018-03-04 17:10:59 -0800

    Initial Commit

---
+++ Volunteer Opportunity Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Volunteer Opportunity Tracker
+//
+//  Created by Cindy Zhang on 3/4/18.
+//  Copyright © 2018 Cindy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
